a3f0f17ef166bc3983559074976f7430cfb21fa7
Kyle Machulis <kyle@nonpolynomial.com>
2017-12-22 08:39:27 -0800

    chore: fix identation differences

--- src/sugarcube2-macros.js
+++ src/sugarcube2-macros.js
@@ -1,65 +1,65 @@
 /*
-	External script loading module.
-	Taken from https://twinery.org/forum/discussion/6628/sugarcube-2-9-0-including-external-javascript-libraries
+  External script loading module.
+  Taken from https://twinery.org/forum/discussion/6628/sugarcube-2-9-0-including-external-javascript-libraries
 */
 (() => {
-	'use strict';
-	window.requestScriptLoad = function (options) {
-		if (options == null || typeof options !== 'object' || !options.src) {
-			return;
-		}
+  'use strict';
+  window.requestScriptLoad = function (options) {
+    if (options == null || typeof options !== 'object' || !options.src) {
+      return;
+    }
 
-		var
-		opts   = Object.assign({ parent : document.head }, options),
-		script = document.createElement('script');
+    var
+    opts   = Object.assign({ parent : document.head }, options),
+    script = document.createElement('script');
 
-		function onLoadOnce(evt) {
-			opts.onload.call(evt.target, evt);
-			script.removeEventListener('load', onLoadOnce);
-		}
+    function onLoadOnce(evt) {
+      opts.onload.call(evt.target, evt);
+      script.removeEventListener('load', onLoadOnce);
+    }
 
-		script.id   = opts.id;
-		script.src  = opts.src;
-		script.type = 'text/javascript';
+    script.id   = opts.id;
+    script.src  = opts.src;
+    script.type = 'text/javascript';
 
-		if (typeof opts.onload === 'function') {
-			script.addEventListener('load', onLoadOnce, false);
-		}
+    if (typeof opts.onload === 'function') {
+      script.addEventListener('load', onLoadOnce, false);
+    }
 
-		opts.parent.appendChild(script);
-	};
+    opts.parent.appendChild(script);
+  };
 
   let buttplugLoadingPromise = new Promise((resolve, reject) => {
-	  requestScriptLoad({
-		  id     : 'lib-buttplug-js',
-		  src    : 'https://cdn.jsdelivr.net/npm/buttplug@0.4.0/dist/web/buttplug.min.js',
-		  onload : function (evt) {
-		    resolve();
-		  }
-	  });
+    requestScriptLoad({
+      id     : 'lib-buttplug-js',
+      src    : 'https://cdn.jsdelivr.net/npm/buttplug@0.4.0/dist/web/buttplug.min.js',
+      onload : function (evt) {
+        resolve();
+      }
+    });
   });
 
-	function mapPayloads(payloads) {
-		let payloadMap = new Map();
-		for (let payload of payloads) {
-			payloadMap.set(payload.name, payload);
-		}
-		return payloadMap;
-	}
+  function mapPayloads(payloads) {
+    let payloadMap = new Map();
+    for (let payload of payloads) {
+      payloadMap.set(payload.name, payload);
+    }
+    return payloadMap;
+  }
 
   Macro.add("buttplugloaded", {
-		tags: null,
-	  handler() {
-		  buttplugLoadingPromise.then(() => Wikifier.wikifyEval(this.payload[0].contents.trim()));
-	  }
+    tags: null,
+    handler() {
+      buttplugLoadingPromise.then(() => Wikifier.wikifyEval(this.payload[0].contents.trim()));
+    }
   });
 
-	Macro.add("buttplugconnectlocal", {
-		tags: ["connecting", "success", "failure"],
-		handler() {
-			let payloadMap = mapPayloads(this.payload);
-			Wikifier.wikifyEval(payloadMap.get("connecting").contents);
-			Wikifier.wikifyEval(payloadMap.get("failure").contents);
-		}
-	});
+  Macro.add("buttplugconnectlocal", {
+    tags: ["connecting", "success", "failure"],
+    handler() {
+      let payloadMap = mapPayloads(this.payload);
+      Wikifier.wikifyEval(payloadMap.get("connecting").contents);
+      Wikifier.wikifyEval(payloadMap.get("failure").contents);
+    }
+  });
 })();
