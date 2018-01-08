c04bb40416a07a5b80f5aca9baa223c2c5259402
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-07 18:46:43 -0800

    feat: Add device storage
    Store devices on setup object

--- src/sugarcube2-macros.js
+++ src/sugarcube2-macros.js
@@ -24,6 +24,7 @@
   // and devices a bit odd, but this is a sex toy library for an interactive
   // fiction engine. We started at odd and headed outward from there.
 	let bpClient;
+  setup.bpDevices = [];
 
   Macro.add("buttplugloaded", {
     tags: null,
@@ -32,6 +33,10 @@
     }
   });
 
+  const deviceAddedCallback = (device) => {
+    setup.bpDevices.push(device);
+  };
+
   Macro.add("buttplugconnectlocal", {
     tags: ["connecting", "success", "failure"],
     async handler() {
@@ -39,11 +44,35 @@
       // Run the connecting block before actually trying to connect
       Wikifier.wikifyEval(payloadMap.get("connecting").contents);
       // TODO Let user name client as argument
-      State.variables.bpClient = new Buttplug.ButtplugClient("Twine Buttplug Client");
-		  bpClient = State.variables.bpClient;
+      setup.bpClient = new Buttplug.ButtplugClient("Twine Buttplug Client");
+		  bpClient = setup.bpClient;
 
       try {
         await bpClient.ConnectLocal();
+        bpClient.addListener('deviceadded', deviceAddedCallback);
+        Wikifier.wikifyEval(payloadMap.get("success").contents);
+      } catch (e) {
+        Wikifier.wikifyEval(payloadMap.get("failure").contents);
+      }
+    }
+  });
+
+  Macro.add("buttplugconnectwebsocket", {
+    tags: ["connecting", "success", "failure"],
+    async handler() {
+      const payloadMap = mapPayloads(this.payload);
+      // Run the connecting block before actually trying to connect
+      Wikifier.wikifyEval(payloadMap.get("connecting").contents);
+      // TODO Let user name client as argument
+      setup.bpClient = new Buttplug.ButtplugClient("Twine Buttplug Client");
+		  bpClient = setup.bpClient;
+
+      try {
+        await bpClient.ConnectWebsocket("wss://192.168.123.2:12345/buttplug");
+        bpClient.addListener('deviceadded', deviceAddedCallback);
+        // Websocket servers may keep state between connections, meaning we
+        // should fetch devices now.
+        await bpClient.RequestDeviceList();
         Wikifier.wikifyEval(payloadMap.get("success").contents);
       } catch (e) {
         Wikifier.wikifyEval(payloadMap.get("failure").contents);
@@ -76,7 +105,7 @@
         return this.error("We need a client object!");
       }
       bpClient.addListener('deviceadded', (device) => {
-				State.temporary.device = device;
+        State.temporary.device = device;
         Wikifier.wikifyEval(this.payload[0].contents.trim());
       });
     }
@@ -98,6 +127,9 @@
   Macro.add("buttplugdisconnect", {
 		tags: null,
     handler() {
+      bpClient.disconnect();
+      bpClient = undefined;
+      setup.bpDevices = [];
       // We currently have no way to detect this. WTF is wrong with me.
       // https://github.com/metafetish/buttplug-js/issues/64
     }
