0541937aa12565f2cdea17155a8026c11d574ea7
Kyle Machulis <kyle@nonpolynomial.com>
2018-01-29 20:22:46 -0800

    feat: Update to buttplug-js 0.5.3
    Fixs devtools web tool exposure

--- src/sugarcube2-macros.js
+++ src/sugarcube2-macros.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const buttplugLoadingPromise = importScripts('https://cdn.jsdelivr.net/npm/buttplug@0.5.2/dist/web/buttplug.min.js').then(() => importScripts('https://cdn.jsdelivr.net/npm/buttplug@0.5.2/dist/web/buttplug-devtools.min.js'));
+  const buttplugLoadingPromise = importScripts('https://cdn.jsdelivr.net/npm/buttplug@0.5.3/dist/web/buttplug.min.js').then(() => importScripts('https://cdn.jsdelivr.net/npm/buttplug@0.5.3/dist/web/buttplug-devtools.min.js'));
 
   // Map multiple payloads from child tags to an es6 <string, object> map.
   function mapPayloads(payloads) {
